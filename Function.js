83bcb7faf36bbb459bf40929c25d0cd873bd7417
MrSun10 <rayyansaidi@outlook.com>
2020-04-09 14:47:01 -0400

    remove message line

--- Function.js
+++ Function.js
@@ -1,19 +1,22 @@
 function hvh() {
     var x = document.getElementById('hvhButtons');
     var y = document.getElementById('hvcButtons');
+    var z = document.getElementById('Message');
     if (y.style.display === 'block') {
         y.style.display = 'none'
     }
+    z.innerHTML = ''
     x.style.display = 'block';
 }
 function hvc() {
     var x = document.getElementById('hvcButtons');
     var y = document.getElementById('hvhButtons');
+    var z = document.getElementById('Message');
     if (y.style.display === 'block') {
         y.style.display = 'none'
     }
+    z.innerHTML = ''
     x.style.display = 'block';
-
 }
 
 function cvc() {
@@ -22,16 +25,26 @@ function cvc() {
     Math.floor(Math.random() * 3)
     var y =
     Math.floor(Math.random() * 3)
+    var z = document.getElementById('hvcButtons');
+    var a = document.getElementById('hvhButtons');
+    var b = document.getElementById('Message');
+    if (z.style.display === 'block') {
+        z.style.display = 'none'
+    }
+    if (a.style.display === 'block') {
+        a.style.display = 'none'    
+    }
+    b.innerHTML = ''
     if (x == 0 && y == 0) {
-        document.getElementById('Message').innerHTML = 'Tie. Computer 1 and 2 both picked rock';
+        document.getElementById('Message').innerHTML = 'Tie. Computer 1 and 2 both picked rock.';
     }
 
     else if (x == 1 && y == 0) {
-        document.getElementById('Message').innerHTML = 'Computer 1 picked Paper, Computer 2 picked rock, Computer 1 wins';
+        document.getElementById('Message').innerHTML = 'Computer 1 picked Paper, Computer 2 picked rock, Computer 1 wins.';
     }
 
     else if (x == 2 && y == 0) {
-        document.getElementById('Message').innerHTML = 'Computer 1 picked Scissors, Computer 2 picked rock, Computer 2 wins';
+        document.getElementById('Message').innerHTML = 'Computer 1 picked scissors, Computer 2 picked rock, Computer 2 wins.';
     }
     
     else if (x == 0 && y == 1) {
@@ -39,11 +52,11 @@ function cvc() {
     }
     
     else if (x == 1 && y == 1) {
-        document.getElementById('Message').innerHTML = 'Tie. Computer 1 and 2 both picked paper';
+        document.getElementById('Message').innerHTML = 'Tie. Computer 1 and 2 both picked paper.';
     }
     
     else if (x == 2 && y == 1) {
-        document.getElementById('Message').innerHTML = 'Computer 1 sissors, Computer 2 picked paper, Computer 1 wins';
+        document.getElementById('Message').innerHTML = 'Computer 1 picked scissors, Computer 2 picked paper, Computer 1 wins.';
     }
     
     else if (x == 0 && y == 2) {
@@ -76,6 +89,10 @@ function cvc() {
     else if (y == 2) {
         document.getElementById('Message').innerHTML = 'Player wins. Computer picked scissors.'
     }
+
+    else {
+        alert('Oops1!')
+    }
 }
 
     function paper() {
@@ -90,19 +107,25 @@ function cvc() {
     else if (y == 2) {
         document.getElementById('Message').innerHTML = 'Computer wins. Computer picked scissors.'
     } 
+
+    else {
+        alert('Oops2!')
+    }
 }
     function scissors() {
         var y = 
     Math.floor(Math.random() * 3)
     if (y == 0) {
-        document.getElementById('Message').innerHTML = 'Player wins. Computer picked rock.'
+        document.getElementById('Message').innerHTML = 'Computer wins. Computer picked rock.'
     }
     else if (y == 1) {
-        document.getElementById('Message').innerHTML = 'Tie. Computer picked paper'
+        document.getElementById('Message').innerHTML = 'Player wins. Computer picked paper'
     }
     else if (y == 2) {
-        document.getElementById('Message').innerHTML = 'Computer wins. Computer picked scissors.'
+        document.getElementById('Message').innerHTML = 'Tie. Computer picked scissors.'
     } 
-}
 
-    
+    else {
+        alert('Oops3!')
+    }
+}   
